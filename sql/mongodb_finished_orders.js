0721a37a046ca41c1bc84e45408c3ed0e081175f
Damgharr <dg@kobra.rocks>
2026-05-18 13:43:39 +0200

    ajout des fichiers de créations et insertion des tables des base de données

---
+++ sql/mongodb_finished_orders.js
@@ -0,0 +1,550 @@
+db.FinishedOrder.insertMany(
+[
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b2"
+        },
+        "orderId": 1,
+        "orderNumber": "ORD-20260517-BAC39",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1779487200000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1779498000000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "120"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "120"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b3"
+        },
+        "orderId": 3,
+        "orderNumber": "ORD-20260518-19768",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777845600000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777856400000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "280"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "280"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b4"
+        },
+        "orderId": 4,
+        "orderNumber": "ORD-20260518-462D8",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778104800000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778115600000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "135"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "135"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b5"
+        },
+        "orderId": 5,
+        "orderNumber": "ORD-20260518-E66E2",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777327200000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777338000000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "100"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "100"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b6"
+        },
+        "orderId": 6,
+        "orderNumber": "ORD-20260518-69827",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1776636000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1776646800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "224"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "224"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b7"
+        },
+        "orderId": 7,
+        "orderNumber": "ORD-20260518-32ECD",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777845600000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777856400000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "96"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "96"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b8"
+        },
+        "orderId": 8,
+        "orderNumber": "ORD-20260518-E97FE",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777932000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777942800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "162"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "162"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581b9"
+        },
+        "orderId": 9,
+        "orderNumber": "ORD-20260518-9A80F",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777845600000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777856400000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "100"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "100"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581ba"
+        },
+        "orderId": 10,
+        "orderNumber": "ORD-20260518-4DE74",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778709600000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778720400000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "168"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "168"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581bb"
+        },
+        "orderId": 11,
+        "orderNumber": "ORD-20260518-6156C",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1776636000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1776646800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "144"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "144"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581bc"
+        },
+        "orderId": 12,
+        "orderNumber": "ORD-20260518-021DC",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1776636000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1776646800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "30"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "30"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581bd"
+        },
+        "orderId": 13,
+        "orderNumber": "ORD-20260518-14504",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777068000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777078800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "30"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "30"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581be"
+        },
+        "orderId": 14,
+        "orderNumber": "ORD-20260518-1B822",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1777500000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1777510800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "30"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "30"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581bf"
+        },
+        "orderId": 15,
+        "orderNumber": "ORD-20260518-D8901",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1780610400000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1780621200000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "30"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "30"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c0"
+        },
+        "orderId": 16,
+        "orderNumber": "ORD-20260518-6A104",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778450400000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778461200000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "30"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "30"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c1"
+        },
+        "orderId": 17,
+        "orderNumber": "ORD-20260518-17615",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778623200000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778634000000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "96"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "96"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c2"
+        },
+        "orderId": 18,
+        "orderNumber": "ORD-20260518-4ED6C",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778709600000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778720400000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "144"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "144"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c3"
+        },
+        "orderId": 19,
+        "orderNumber": "ORD-20260518-98799",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778796000000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778806800000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "168"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "168"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c4"
+        },
+        "orderId": 20,
+        "orderNumber": "ORD-20260518-8AA63",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1778882400000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1778893200000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "30"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "30"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c5"
+        },
+        "orderId": 21,
+        "orderNumber": "ORD-20260518-83070",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1779055200000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1779066000000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "80"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "80"
+        }
+    },
+    {
+        "_id": {
+            "$oid": "6a0ad0cf34366aefb40581c6"
+        },
+        "orderId": 2,
+        "orderNumber": "ORD-20260518-903E8",
+        "orderedAt": {
+            "$date": {
+                "$numberLong": "1776463200000"
+            }
+        },
+        "finishedAt": {
+            "$date": {
+                "$numberLong": "1776474000000"
+            }
+        },
+        "menuPrice": {
+            "$numberDecimal": "198"
+        },
+        "deliveryPrice": {
+            "$numberDecimal": "0"
+        },
+        "totalPrice": {
+            "$numberDecimal": "198"
+        }
+    }
+]
+);
